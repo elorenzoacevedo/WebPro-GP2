b96230e592145cdae9d0629bf4ffd1656d78d03f
jufromda75 <99289366+jufromda75@users.noreply.github.com>
2023-03-28 12:58:49 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
-<?php 
-    //Initialize session user
-    session_start();
-    $_SESSION['user'] = $_GET['name'];
-?>
 <html>
 <head>
     <Title> Deal or No Deal</Title>
     <link rel="stylesheet" type="text/css" href="./style.css">
-    <link rel="icon" type="image/x-icon" href="favicon.jpg">
 </head>
 <body>
-    <img src="deal-no.gif" alt="dealornodeal" class="center">    
+    <h1 class="center">Welcome To Deal Or No Deal!</h1>
+    <div >
+        <img src="deal-no.gif" alt="dealornodeal">
+        <img src="pic24.jpg"alt="14pic" >
+    </div>
+    
+    
+    <section id ="section" class="center">
     <a href="./game.php"><button id="startbtn">START GAME</button></a>
     <p class="start">
-        <button type="button"><a href="./leaderboard.php">Leaderboard</a></button>  
+        <button type="button"><a href="">Leaderboard</a></button>  
     </p>
+    </section>    
+    
 </body>
 </html>
