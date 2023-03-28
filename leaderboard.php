be75ce7087340ce925a9a553fc8231bc20b2a404
elorenzoacevedo <91859156+elorenzoacevedo@users.noreply.github.com>
2023-03-28 08:20:59 -0400

    leaderboard bug fix

--- leaderboard.php
+++ leaderboard.php
@@ -5,12 +5,14 @@ echo "<tr id =title>Leaderboard</tr>";
 
 while (!feof($players)) {
     $line = fgets($players);
-    $playerData = explode(",", $line);
+        if($line != ""){
+        $playerData = explode(",", $line);
 
-    echo "<tr>";
-    echo "<td>" . $playerData[0] . "</td>";
-    echo "<td>" . $playerData[1] . "</td>";
-    echo "</tr>";
+        echo "<tr>";
+        echo "<td>" . $playerData[0] . "</td>";
+        echo "<td>" . $playerData[1] . "</td>";
+        echo "</tr>";
+    }
 }
 
 echo "</table>";
